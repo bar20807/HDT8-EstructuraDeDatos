ea14005f60acde7858d9d3a8702957c2d97bcc66
bar20807 <70623939+bar20807@users.noreply.github.com>
2021-04-22 22:36:20 -0600

    JUnit Test Finalizado

--- VectorHeapTest.java
+++ VectorHeapTest.java
@@ -1,11 +1,8 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 /**
  * 
- * @author José Rodrigo Barrera García Carnet: 20807
+ * @author JosÃ© Rodrigo Barrera GarcÃ­a Carnet: 20807
  * Universidad del Valle 
  * Pruebas JUnit
  *
@@ -18,7 +15,7 @@ class VectorHeapTest {
      */
 	 @Test
 	    public void testAdd() {
-	        System.out.println("Añadido");
+	        System.out.println("AÃ±adido");
 	        VectorHeap<Paciente> instance = new VectorHeap<>();
 	        Paciente paciente = new Paciente("Rodrigo Prueba","alergia",'F');//Object value = null;
 	        //VectorHeap instance = new VectorHeap();
